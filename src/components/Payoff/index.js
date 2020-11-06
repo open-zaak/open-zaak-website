beadff056213d28ee6c751048e31b14b92365f60
Felix Faassen <felix.faassen@protonmail.com>
2020-11-06 16:15:57 +0100

    NL payoff rewrite

--- src/components/Payoff/index.js
+++ src/components/Payoff/index.js
@@ -9,7 +9,7 @@ const Payoff = () => (
       <StyledTextContainer>
         <StyledHeading>OpenZaak</StyledHeading>
         <StyledParagraph>Online dienstverlening voor burgers</StyledParagraph>
-        <StyledSubParagraph>OpenZaak is een open source software die de nationale standaard voor zaakgericht werken implementeert (de ZGW-API’s). OpenZaak maakt het mogelijk voor gemeenten om sneller en eenvoudiger online diensten aan te kunnen bieden aan haar inwoners en noodzakelijke data te kunnen uitwisselen tussen gemeenten.</StyledSubParagraph>
+        <StyledSubParagraph>OpenZaak maakt het mogelijk voor gemeenten om sneller en eenvoudiger online diensten aan te kunnen bieden aan haar inwoners en noodzakelijke data te kunnen uitwisselen tussen gemeenten.</StyledSubParagraph>
         <StyledSubParagraph><a href="/#over-openzaak">Lees hier meer over OpenZaak</a></StyledSubParagraph>
       </StyledTextContainer>
     </StyledIntro>
