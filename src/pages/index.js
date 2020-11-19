c67e4ee98dd9be3d71ba021adc3e70de23e9041a
felixfaassen <felix@publiccode.net>
2020-11-19 11:52:13 +0100

    rewrite integration highlight main page NL

--- src/pages/index.js
+++ src/pages/index.js
@@ -106,7 +106,7 @@ const IndexPage = ({ location }) => (
           </Box>
           <Box width={3/5}>
             <h2>Syteem integraties</h2>
-            <p>Omdat OpenZaak open source is kunnen leveranciers OpenZaak integreren in hun document en content management systemen en heeft u de vrijheid om gebruik te maken van uw bestaande of nieuwe applicaties. Zo kan OpenZaak koppelen met onder andere: Contezza DMS en Alfreso.</p>
+            <p>Omdat OpenZaak open source is kunnen leveranciers OpenZaak systeem integraties ontwikkelen voor hun document en content management systemen. Zo heeft u de vrijheid om gebruik te maken van bestaande of nieuwe applicaties in combinatie met OpenZaak. OpenZaak koppelt onder andere met Contezza DMS en Alfreso.</p>
           </Box>
         </Flex>
       </Section>
