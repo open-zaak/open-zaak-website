5b29d232a3eae5402de15c7dafae7d0a59a0c83b
felixfaassen <felix@publiccode.net>
2020-11-20 16:22:06 +0100

    add new logo to payoff header

--- src/components/Logos/index.js
+++ src/components/Logos/index.js
@@ -9,12 +9,16 @@ import gemeenteArnhem from './images/gemeente-arnhem.png'
 import gemeenteDelft from './images/gemeente-delft.png'
 import gemeenteDenBosch from './images/gemeente-den-bosch.png'
 import gemeenteHaarlem from './images/gemeente-haarlem.png'
+import gemeenteRotterdam from './images/gemeente-rotterdam.png'
+import gemeenteTilburg from './images/gemeente-tilburg.png'
 
 import vng from './images/vng.png'
 import dimpact from './images/dimpact.png'
 import maykin from './images/maykinmedia.png'
 import opensatisfaction from './images/opensatisfaction.png'
 import excellence from './images/excellence.png'
+import contezza from './images/contezza.png'
+
 import sed from './images/sed.png'
 
 import foundationForPublicCode from './images/foundation-for-public-code.png'
@@ -29,11 +33,14 @@ const Logos = () => (
     <a href="http://delft.nl/" target="_blank" rel="noopener noreferrer"><img src={gemeenteDelft} height="45" alt="Logo Gemeente Delft" /></a>
     <a href="https://www.s-hertogenbosch.nl/" target="_blank" rel="noopener noreferrer"><img src={gemeenteDenBosch} height="40" alt="Logo Gemeente Den Bosch" /></a>
     <a href="https://www.haarlem.nl/" target="_blank" rel="noopener noreferrer"><img src={gemeenteHaarlem} height="40" alt="Logo Gemeente Haarlem" /></a>
+    <a href="https://www.rotterdam.nl/" target="_blank" rel="noopener noreferrer"><img src={gemeenteRotterdam} height="40" alt="Logo Gemeente Rotterdam" /></a>
+    <a href="https://www.tilburg.nl/" target="_blank" rel="noopener noreferrer"><img src={gemeenteTilburg} height="40" alt="Logo Gemeente Tilburg" /></a>
 
 
     <a href="https://www.dimpact.nl" target="_blank" rel="noopener noreferrer"><img src={maykin} height="40" alt="Logo Maykin Media B.V." /></a>
     <a href="https://www.opensatisfaction.nl" target="_blank" rel="noopener noreferrer"><img src={opensatisfaction} height="40" alt="Logo Open Satisfaction B.V." /></a>
     <a href="https://exxellence.nl/" target="_blank" rel="noopener noreferrer"><img src={excellence} height="40" alt="Logo excellence" /></a>
+    <a href="https://contezza.nl/" target="_blank" rel="noopener noreferrer"><img src={contezza} height="40" alt="Logo contezza" /></a>
 
     <a href="https://www.dimpact.nl" target="_blank" rel="noopener noreferrer"><img src={dimpact} height="40" alt="Logo Vereniging Dimpact" /></a>
 
--- src/pages/contact.en.md
+++ src/pages/contact.en.md
@@ -8,30 +8,30 @@ Do you want to join the community and use OpenZaak?
 
 ### Representatives of the OpenZaak core group
 
-Niels Lindeboom <br/>
-Director Municipal Collaboration<br/>
-Dimpact<br/>
+Niels Lindeboom  
+Director Municipal Collaboration  
+Dimpact  
 [niels.lindeboom@dimpact.nl](niels.lindeboom@dimpact.nl)
 
 ### Representatives from participating municipalities
-Lazo Bozarov<br/>
-Senior Supply Manager Digital Service<br/>
-Municipality of Utrecht<br/>
+Lazo Bozarov  
+Senior Supply Manager Digital Service  
+Municipality of Utrecht  
 [l.bozarov@utrecht.nl](mailto:l.bozarov@utrecht.nl)
 
 ### Representatives of OpenZaak vendors
-Joeri Bekker<br/>
-Founder Maykin Media BV<br/>
+Joeri Bekker  
+Founder Maykin Media BV  
 [joeri.bekker@maykinmedia.nl](mailto:joeri.bekker@maykinmedia.nl)
 
-Tahir Malik<br/>
-Technical Director<br/>
-Contezza<br/>
+Tahir Malik  
+Technical Director  
+Contezza  
 [tahir@contezza.nl](mailto:tahir@contezza.nl)
 
 ### Representative of VNG Realisatie (Dutch Association of Municipalities)
-Hugo ter Doest<br/>
-Product owner APIs for business-oriented work, VNG Realisatie<br/>
+Hugo ter Doest  
+Product owner APIs for business-oriented work, VNG Realisatie  
 [hugo.terdoest@dimpact.nl](mailto:hugo.terdoest@dimpact.nl)
 
 ### Get in touch with The OpenZaak Community
